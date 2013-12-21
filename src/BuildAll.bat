f6b7138347283c46835b2f2b4dc4ef48ebf0952b
Michal Kotelba <michal@kotelba.us>
2013-12-21 15:49:58 -0500

    - Refactored Makefile to use variables, create/use a temporary build directory, and (in general) comply closer with GNU Make standards. - Removed install.js (no longer required for Firefox extensions). - Removed extraneous/obsolete files.

--- src/BuildAll.bat
+++
@@ -1,6 +0,0 @@
-mkdir chrome
-del chrome\mozimage.jar
-del mozimage.xpi
-zip chrome\mozimage.jar -r content locale skin -x CVS
-zip mozimage.xpi -r chrome COPYING install.js install.rdf chrome.manifest
-pause

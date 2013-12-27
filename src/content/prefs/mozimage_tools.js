6b003e76c468ac2f8b81dc1f1cfab120f391cfee
Luca Minuti <lithian@gmail.com>
2013-12-27 16:50:10 +0100

    Added the mozimage namespace for the tools page of the program settings

--- src/content/prefs/mozimage_tools.js
+++ src/content/prefs/mozimage_tools.js
@@ -1,84 +1,99 @@
-prefs = top.prefs;
-var mozImageBundle = srGetStrBundle("chrome://mozimage/locale/mozimage.properties");
+mozimage.define('mozimage.PrefsTools', {
 
-/*
- **
- ** Prefs window event handlers
- **
- */
+	prefs : top.prefs,
 
-function openFilerConvert() {
-	try {
-		var selectdir = mozImageBundle.GetStringFromName("choosedir");
-		var nsIFilePicker = Components.interfaces.nsIFilePicker;
-		var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-		fp.init(window, selectdir, nsIFilePicker.modeOpen);
-		fp.appendFilters(nsIFilePicker.filterAll);
-		var res = fp.show();
-		if (res == nsIFilePicker.returnOK) {
-			var thepath = fp.file;
-			//prefs.setHomeDir(thefile.path);
-			if (navigator.platform == "Win32")
-				document.getElementById('convertpath-text').value = "'" + thepath.path + "'";
-			else
-				document.getElementById('convertpath-text').value = thepath.path;
+	mozImageBundle : null,
+
+	init : function () {
+		this.mozImageBundle = srGetStrBundle("chrome://mozimage/locale/mozimage.properties");
+		mozimage.addEventListener(window, 'pagehide', this.prefs_close, this);
+
+		mozimage.addEventListener('open-filerconvert-button', 'command', this.openFilerConvert, this);
+
+		this.prefs_load();
+	},
+
+	prefs_load : function () {
+		var editorsName = this.prefs.getEditorsName();
+		var editorsPath = this.prefs.getEditorsPath();
+		var macroName = this.prefs.getMacroName();
+		var macroCode = this.prefs.getMacroCode();
+		var edit, i;
+
+		var convertPath = document.getElementById("convertpath-text");
+		if (convertPath) convertPath.value = this.prefs.getConvertPath();
+
+		for (i = 1; i <= 4; i++) {
+			edit = document.getElementById("editorname" + i + "-text");
+			if (edit) edit.value = editorsName[i - 1];
+			edit = document.getElementById("editorpath" + i + "-text");
+			if (edit) edit.value = editorsPath[i - 1];
 		}
-	} catch (e) {
-		alert(e);
-	}
-}
 
-function prefs_load() {
-	var editorsName = prefs.getEditorsName();
-	var editorsPath = prefs.getEditorsPath();
-	var macroName = prefs.getMacroName();
-	var macroCode = prefs.getMacroCode();
-
-	var convertPath = document.getElementById("convertpath-text");
-	if (convertPath) convertPath.value = prefs.getConvertPath();
-
-	for (var i = 1; i <= 4; i++) {
-		var edit = document.getElementById("editorname" + i + "-text");
-		if (edit) edit.value = editorsName[i - 1];
-		var edit = document.getElementById("editorpath" + i + "-text");
-		if (edit) edit.value = editorsPath[i - 1];
-	}
-
-	for (var i = 0; i < 10; i++) {
-		var edit = document.getElementById("macroname" + i + "-text");
-		if (edit) edit.value = macroName[i];
-		var edit = document.getElementById("macrocode" + i + "-text");
-		if (edit) edit.value = macroCode[i];
-	}
-}
-
-function prefs_close() {
-	try {
-		var convertPath = document.getElementById("convertpath-text").value;
-		var editorsName = new Array();
-		var editorsPath = new Array();
-		var macroName = new Array();
-		var macroCode = new Array();
-
-		for (var i = 1; i <= 4; i++) {
-			var edit = document.getElementById("editorname" + i + "-text");
-			editorsName[i - 1] = edit.value;
-			var edit = document.getElementById("editorpath" + i + "-text");
-			editorsPath[i - 1] = edit.value;
+		for (i = 0; i < 10; i++) {
+			edit = document.getElementById("macroname" + i + "-text");
+			if (edit) edit.value = macroName[i];
+			edit = document.getElementById("macrocode" + i + "-text");
+			if (edit) edit.value = macroCode[i];
 		}
-		for (var i = 0; i < 10; i++) {
-			var edit = document.getElementById("macroname" + i + "-text");
-			macroName[i] = edit.value;
-			var edit = document.getElementById("macrocode" + i + "-text");
-			macroCode[i] = edit.value;
-		}
-		prefs.setConvertPath(convertPath);
-		prefs.setEditorsName(editorsName);
-		prefs.setEditorsPath(editorsPath);
-		prefs.setMacroName(macroName);
-		prefs.setMacroCode(macroCode);
-	} catch (e) {
-		alert(e);
-	}
-}
+	},
 
+	prefs_close : function () {
+		try {
+			var convertPath = document.getElementById("convertpath-text").value;
+			var editorsName = [];
+			var editorsPath = [];
+			var macroName = [];
+			var macroCode = [];
+			var edit, i;
+
+			for (i = 1; i <= 4; i++) {
+				edit = document.getElementById("editorname" + i + "-text");
+				editorsName[i - 1] = edit.value;
+				edit = document.getElementById("editorpath" + i + "-text");
+				editorsPath[i - 1] = edit.value;
+			}
+			for (i = 0; i < 10; i++) {
+				edit = document.getElementById("macroname" + i + "-text");
+				macroName[i] = edit.value;
+				edit = document.getElementById("macrocode" + i + "-text");
+				macroCode[i] = edit.value;
+			}
+			this.prefs.setConvertPath(convertPath);
+			this.prefs.setEditorsName(editorsName);
+			this.prefs.setEditorsPath(editorsPath);
+			this.prefs.setMacroName(macroName);
+			this.prefs.setMacroCode(macroCode);
+		} catch (e) {
+			mozimage.showError(e);
+		}
+	},
+
+	openFilerConvert : function () {
+		try {
+			var selectdir = this.mozImageBundle.GetStringFromName("choosedir");
+			var nsIFilePicker = Components.interfaces.nsIFilePicker;
+			var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+			fp.init(window, selectdir, nsIFilePicker.modeOpen);
+			fp.appendFilters(nsIFilePicker.filterAll);
+			var res = fp.show();
+			if (res == nsIFilePicker.returnOK) {
+				var thepath = fp.file;
+				//this.prefs.setHomeDir(thefile.path);
+				if (navigator.platform == "Win32")
+					document.getElementById('convertpath-text').value = "'" + thepath.path + "'";
+				else
+					document.getElementById('convertpath-text').value = thepath.path;
+			}
+		} catch (e) {
+			mozimage.showError(e);
+		}
+	}
+
+});
+
+(function () {
+	window.addEventListener('load',function() {
+		new mozimage.PrefsTools();
+	});
+})();
